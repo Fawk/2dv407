ef5172c6fee30245d25910df5e376df711100420
ok222ax <ok222ax@student.lnu.se>
2014-11-11 17:49:03 +0100

    build

--- src/components/app.js
+++ src/components/app.js
@@ -3,13 +3,13 @@ var React = require('react');
 var CommentBox = React.createClass({displayName: 'CommentBox',
   render: function() {
     return (
-      React.createElement(React.DOM.div, {className: "commentBox"}, 
+      React.createElement('div', {className: "commentBox"}, 
         "Hello, world! I am a CommentBox."
       )
     );
   }
 });
 React.renderComponent(
-  React.createElement(CommentBox, "hej"),
+  React.createElement(CommentBox, null),
   document.getElementById('content')
 );
