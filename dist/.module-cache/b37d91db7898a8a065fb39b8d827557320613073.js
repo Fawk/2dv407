f8efb867b7b5b6db1f44040826585f4b378d07cd
ok222ax <ok222ax@student.lnu.se>
2014-11-11 17:09:35 +0100

    build

--- dist/.module-cache/b37d91db7898a8a065fb39b8d827557320613073.js
+++ dist/.module-cache/b37d91db7898a8a065fb39b8d827557320613073.js
@@ -18660,7 +18660,7 @@ var React = require('react');
 var CommentBox = React.createClass({displayName: 'CommentBox',
   render: function() {
     return (
-      React.createElement("div", {className: "commentBox"}, 
+      React.createElement(React.DOM.div, {className: "commentBox"}, 
         "Hello, world! I am a CommentBox."
       )
     );
