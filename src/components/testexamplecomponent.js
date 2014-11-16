69aa07ca42eeaaba1576ad2b495ca7cb5c0ad0af
ok222ax <ok222ax@student.lnu.se>
2014-11-16 15:36:44 +0100

    jest

---
+++ src/components/testexamplecomponent.js
@@ -0,0 +1,26 @@
+/** @jsx React.DOM */
+
+// This component isn't used in the guild, but is used as a demonstration of how tests work.
+
+var React = require('react/addons');
+var CheckboxWithLabel = React.createClass({
+  getInitialState: function() {
+    return { isChecked: false };
+  },
+  onChange: function() {
+    this.setState({isChecked: !this.state.isChecked});
+  },
+  render: function() {
+    return (
+      <label>
+        <input
+          type="checkbox"
+          checked={this.state.isChecked}
+          onChange={this.onChange}
+        />
+        {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+      </label>
+    );
+  }
+});
+module.exports = CheckboxWithLabel;
